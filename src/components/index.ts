5483b9cd4c9ff0d651214f864114b29c59103780
VITALIJ <Vitaliythesecond@gmail.com>
2022-04-20 23:39:42 +0300

    components templates

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
-export { default as Button } from './button'
+export { default as Button } from './button';
+export { default as Wrapper } from './wrapper';
+export { default as List } from './list';
+export { default as Preview } from './preview';
+
