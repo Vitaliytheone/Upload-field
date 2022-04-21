2890531bd2976a016a98fe7faef874ded600ac5b
VITALIJ <Vitaliythesecond@gmail.com>
2022-04-21 14:44:48 +0300

    logging

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const Button = () => {
     const ref = useRef(null);
     const [files, setFiles] = useState({});
+    console.info(files);
 
     return <input type="file" ref={ref} />;
 };
